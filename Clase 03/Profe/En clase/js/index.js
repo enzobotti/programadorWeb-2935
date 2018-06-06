9293ab75f70abc654f09b8058cfe3d4f556dac9a
Enzolaptop <eiobutti@gmail.com>
2018-06-05 21:09:13 -0300

    modificacion de archivo indexjsprofe

--- Clase 03/Profe/En clase/js/index.js
+++ Clase 03/Profe/En clase/js/index.js
@@ -8,10 +8,67 @@
 
 // # Bucles e iteración
 
+console.log('init app')
+
+
 // 1. Sentencias de blucle
 //  - while
+// var couter =
+//     while (couter > 6) {
+//         console.log(couter)
+//         couter = couter + 1
+//     }
+// var name = prompt('ingrese su nombre')
+
+
+// while (!name) {
+//     name = prompt('ingrese su nombre')
+// }
+// console.log('hola ' + name)
+
 //  - do while
+
+// var name
+
+// do {
+//     name = prompt('ingrese su nombre')
+// } while (!name)
+
+// console.log('Hola ' + name)
+
+
+// while (!name) {
+//     name = prompt('ingrese su nombre')
+// }
+// console.log('hola ' + name)
+
+
 //  - for
+// // (i++) === 
+// for (var i = 0; i < 3; i++) {
+//     console.log(i)
+// }
+// var students = ('Constanza', 'Javier', 'Marcela')
+
+// var student
+
+// for (var i = 0; i < 3; i++) {
+//     student = students[i]
+//     console.log(student)
+var students = ('Constanza', 'Javier', 'Marcela', 'adrian')
+
+var student
+for (var i = 0; i < 3; i++) {
+    student = students[i]
+
+    if (student !== 'adrian') {
+        console.log('te encontre constanza en la posicion '
+            i)
+
+    }
+}
+
+
 // 2. Sentencias de corte
 //  - break
 //  - continue
